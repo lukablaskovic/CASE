8b0d9220481e97c2a2d60824fdafa5f013fa52ff
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2024-03-07 14:55:17 +0100

    Progress

--- evaluator.py
+++ evaluator.py
@@ -3,7 +3,7 @@ import asyncio
 import json
 import os
 import dotenv
-from helpers.file_operations import export_dict_to_csv, read_file_content
+from helpers.file_operations import export_dict_to_csv
 
 dotenv.load_dotenv()
 
@@ -11,14 +11,13 @@ client = AsyncOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
 )
 
-def evaluate(instructions, tasks_text, student_code, student_email, exam_password):
+async def call_evaluator(instructions, tasks_text, student_code, student_email, exam_password):
     prompt = f"{instructions}\n\n###{tasks_text}\n\n### Student's Code:\n{student_code}\n\n### Evaluation:\nPlease return a structured response with the following format: '{{\"task_1\": \"task_1_points\", \"task_2\": \"task_2_points\", \"total_points\": \"total_points_here\", \"feedback\": \"feedback_here\"}}'. Please provide task points for each task."
     
     json_response = {}
     json_response['student_email'] = student_email
     json_response['exam_password'] = exam_password
-    
-    """
+
     response  = await client.chat.completions.create(
         model= "gpt-4-turbo-preview",
         response_format={"type": "json_object"},
@@ -28,28 +27,21 @@ def evaluate(instructions, tasks_text, student_code, student_email, exam_passwor
         ],
         temperature=0.2,
     )
-    """
     
-    response_data = {'student_email': 'lblaskovi@student.unipu.hr', 'exam_password': 'matematika', 'task_1': '5', 'task_2': '4.5', 'total_points': '9.5', 'feedback': 'ZADATAK 1: Funkcija ispravno implementira Pitagorin poučak i vraća očekivani rezultat. ZADATAK 2: Funkcija ispravno provjerava je li ulaz broj i vraća 10 brojeva većih od n, ali nije specificirano kako se ispisuju brojevi u binarnom obliku ili zaokruženi na dvije decimale u konzolu.'}
+    #response_data = {'student_email': 'lblaskovi@student.unipu.hr', 'exam_password': 'matematika', 'task_1': '5', 'task_2': '4.5', 'total_points': '9.5', 'feedback': 'ZADATAK 1: Funkcija ispravno implementira Pitagorin poučak i vraća očekivani rezultat. ZADATAK 2: Funkcija ispravno provjerava je li ulaz broj i vraća 10 brojeva većih od n, ali nije specificirano kako se ispisuju brojevi u binarnom obliku ili zaokruženi na dvije decimale u konzolu.'}
     
     output_file = f"results/{exam_password}/{student_email}.csv"
     
-    #response_data = json.loads(response.choices[0].message.content) 
+    response_data = json.loads(response.choices[0].message.content) 
     json_response.update(response_data) 
     
     export_dict_to_csv(json_response, output_file)
     
     return json_response
 
-instructions = read_file_content("instructions.txt")
 
-print("instructions", instructions)
 
-tasks_text = """
-Tasks are as follows:
-**1.** Napišite funkciju `hipotenuza(duzinaA, duzinaB)` koja prima dužine dvije katete pravokutnog trokuta. Funkcija treba izračunati i vratiti dužinu hipotenuze primjenjujući Pitagorin poučak, koji glasi: `c=√(a²+b²)`, gdje su `a` i `b` dužine kateta, a `c` dužina hipotenuze. Ispiši rezultat u formatu `"Dužina hipotenuze je: [hipotenuza]"`. Za implementaciju koristite metode iz `Math` objekta. MAX_TASK_POINTS=5
-**2.** Napišite funkciju proizvoljnog naziva koja prima broj `n`. Funkcija provjerava je li `n` broj, ako nije vraća poruku `"Nije broj!"`. Ako je broj, funkcija vraća 10 brojeva većih od `n` u formatu: `"Broj 1: [n+1], Broj 2: [n+2], ..., Broj 10: [n+10]"`. Ako su `[Broj 1 - Broj 10]` decimalni brojevi, zaokružite ih na dvije decimale i ispišite ih u tom formatu u konzolu. Ako su `[Broj 1 - Broj 10]` cijeli brojevi, pretvorite ih u binarni oblik i ispišite u konzolu. MAX_TASK_POINTS=5
-"""
+
 
 student_code = """
 #ZADATAK_1
@@ -88,6 +80,6 @@ console.log(fun(5.5));
 student_email = "lblaskovi@student.unipu.hr"
 exam_password = "matematika"
 
-evaluation = evaluate(instructions=instructions, tasks_text=tasks_text, student_code=student_code, student_email=student_email, exam_password=exam_password)
+#evaluation = evaluator(instructions=instructions, tasks_text=tasks_text, student_code=student_code, student_email=student_email, exam_password=exam_password)
 
-print("RESULT:", evaluation)
+#print("RESULT:", evaluation)
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import asyncio
+from functools import wraps
+
 import os
+import json
 import typer
 from typing_extensions import Annotated
-from educoder import educoder_fetch, educoder_fetch_exam_solutions, print_json, extract_code_from_solutions, solutions_extracted
+
 from rich.console import Console
 from rich.spinner import Spinner
 from rich.markdown import Markdown
 from rich import print
+
+from educoder import educoder_fetch, educoder_fetch_exam_solutions, print_json, extract_code_from_solutions, solutions_extracted
+from evaluator import call_evaluator
+from helpers.file_operations import read_file_content
 
 app = typer.Typer()
 console = Console()
@@ -15,6 +23,13 @@ app.add_typer(educoder_connector, name="educoder", help="Commands to interact wi
 
 default_app = typer.Typer()
 app.add_typer(default_app, name="CASE", help="CASE - Coding Assessment & Scoring Engine")
+
+def typer_async(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
 
 def description():
     description = """
@@ -97,18 +112,42 @@ def extract_solutions(exam: Annotated[str, typer.Argument(help="Enter exam code"
     result = extract_code_from_solutions(exam_folder, js, html)
     print(result)
 
+def concatenate_js_code(json_data):
+    data_dict = json.loads(json_data) 
+    code_snippets = data_dict.get('code_snippets', [])
+    return '\n\n'.join([snippet['js_code'] for snippet in code_snippets if 'js_code' in snippet and snippet['js_code'].strip()])
+
+async def evaluate_solutions(code_folder, exam):
+    instructions = read_file_content("instructions.txt")
+    tasks_text = read_file_content("text_a_new_hope.txt")
+    
+    for filename in os.listdir(code_folder):
+        if filename.startswith('js_code') and filename.endswith('.json'):
+            filepath = os.path.join(code_folder, filename)
+            json_data = read_file_content(filepath)
+            student_code = concatenate_js_code(json_data)
+            
+
+            student_email = filename[len('js_code_'):-len('.json')]
+
+            await call_evaluator(instructions=instructions, tasks_text=tasks_text, student_code=student_code, student_email=student_email, exam_password=exam)
+
+
 @default_app.command()
-def evaluate(exam: str):
-    if (not solutions_extracted(exam)):
-        typer.echo("Exam solutions not extracted. Please run the 'extract-solutions' command first.")
-        return
-    typer.echo(f"Evaluating solutions for exam '{exam}'")
-    
-    
+@typer_async
+async def evaluate(exam: str):
+    code_folder = f"exams/{exam}/code_solutions"
+    if not solutions_extracted(exam):
+        console.print("Exam solutions not extracted or no solutions available. Please run the 'educoder extract-solutions' command first.")
+        return         
+    print(f"Evaluating solutions for exam '{exam}'...")
+    await evaluate_solutions(code_folder, exam)
+
+
 
 @default_app.command()
 def test(username: str):
     typer.echo(f"User {username} logged in")
 
 if __name__ == "__main__":
-    app()
+    typer.run(app())
